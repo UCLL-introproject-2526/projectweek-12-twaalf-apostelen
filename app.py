c10c5ede2bde26ba55d107a93f239406d71142ec
FrancisAdesiyan <r1002719@ucll.be>
2025-12-15 14:30:36 +0100

    class app.py

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import pygame
 
 
-# Initialize Pygame
+
+# Create window with given 
+def create_main_surface():
+    screen_size = (1024, 768)
+
+    pygame.display.set_mode(screen_size)
+
 pygame.init()
 
-# Tuple representing width and height in pixels
-screen_size = (1024, 768)
-
-# Create window with given size
-pygame.display.set_mode(screen_size)
+screen = create_main_surface()
