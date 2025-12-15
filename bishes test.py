0861dc109c893839799ce4387f708bce354e2a36
Bishes Kandel <Bishes.Kandel@student@ucll.be>
2025-12-15 22:09:28 +0100

    bishes

---
+++ bishes test.py
@@ -0,0 +1,50 @@
+
+import pygame
+
+
+def main(): 
+    screen = pygame.display.set_mode((1024 , 768))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return False
+        screen.fill((0,0,0))
+        pygame.draw.circle(screen,(255,0,0),(500,500) ,20)
+        pygame.display.flip()
+        
+
+def create_main_surface():
+    screen_size = (1024, 768)
+    pygame.display.set_mode(screen_size)
+    return screen_size
+
+main()
+# import pygame
+
+
+# def main(): 
+#     screen_size = (1024, 768)
+#     screen = pygame.display.set_mode(screen_size)
+    
+#     while True:
+#         screen.fill((0,0,0))
+#         pygame.draw.circle(screen,(255,0,0),(500,500) ,20)
+#         pygame.display.flip()
+
+        
+
+
+# def create_main_surface():
+#     screen_size = (1024, 768)
+#     screen = pygame.display.set_mode(screen_size)
+#     screen.fill((0,0,0))
+#     while True:
+#         pygame.draw.circle(screen,(255,0,0),(500,500) ,20)
+#         pygame.display.flip()
+    
+
+# pygame.init()
+
+
+   
+# main()
