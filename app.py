b52ae2b54548a64d29721b42a06de7881d8faf39
Bishes Kandel <Bishes.Kandel@student@ucll.be>
2025-12-15 14:40:00 +0100

    main + infiite loop

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import pygame
 
 
+def main(): 
+    while True:
+        create_main_surface()
 
-# Create window with given 
 def create_main_surface():
-    while True :
-        screen_size = (1024, 768)
-
-        pygame.display.set_mode(screen_size)
+    screen_size = (1024, 768)
+    pygame.display.set_mode(screen_size)
 
 pygame.init()
 
-screen = create_main_surface()
+screen = main()
