2be02b73adb20a26352edab358fba2d54aa3e042
Thijmen van Wijk <thijmen.vanwijk@student.ucll.be>
2025-12-16 09:42:54 +0100

    bewegend vierkant

--- game.py
+++ game.py
@@ -1,13 +1,37 @@
+
+
 import pygame
 
-def main(): 
-    screen = pygame.display.set_mode((1024 , 768))
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return False
-        screen.fill((0,0,0))
-        pygame.draw.circle(screen,(255,0,0),(500,500) ,20)
-        pygame.display.flip()
-        
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+clock = pygame.time.Clock()
+
+player_rect = pygame.Rect(300, 200, 50, 50)
+speed = 10
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        player_rect.x -= speed
+    if keys[pygame.K_RIGHT]:
+        player_rect.x += speed
+    if keys[pygame.K_UP]:
+        player_rect.y -= speed
+    if keys[pygame.K_DOWN]:
+        player_rect.y += speed
+
+    screen.fill("black")
+    pygame.draw.rect(screen, "blue", player_rect)
+    pygame.display.flip()
+    clock.tick(60)
+
+pygame.quit()
+
+
+
 
